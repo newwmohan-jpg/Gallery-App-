cbceb01d0888e4f61f85544e3ae470f6c61b1981
newwmohan-jpg <newwmohan@gmail.com>
2026-07-28 00:24:46 +0530

    added functionality to the next and previous button ,

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import axios from 'axios'
 const App = () => {
 
 const [data0, setData0] = useState([])
-
+const [page, setPage] = useState(2)
 
   const getData = async()=>{
- const response =  await axios.get("https://picsum.photos/v2/list?page=2&limit=30")
+ const response =  await axios.get(`https://picsum.photos/v2/list?page=${page}&limit=10`)
 
  setData0(response.data)
 
   }
+   useEffect(() => {
+     getData()
+   }, [page])
+   
  let printData = 'no img available'
 
  if(data0.length>0){
@@ -21,9 +25,11 @@ const [data0, setData0] = useState([])
     </div>
   })
  }
-   getData()
+  
+
 
   return (
+
     <div className="bg-black text-white p-4">
  
  
@@ -31,8 +37,12 @@ const [data0, setData0] = useState([])
         {printData}
       </div>
       <div className="flex justify-around mt-5">
-        <button className='bg-green-900 active:bg-black text-white px-4 rounded '>Prev</button>
-        <button className='bg-green-900 text-white px-4 active:bg-black  rounded '>Next</button>
+        <button className='bg-green-900 active:bg-black text-white px-4 rounded ' onClick={()=>{
+setPage(page+1)
+        }}>Prev</button>
+        <button className='bg-green-900 text-white px-4 active:bg-black  rounded ' onClick={()=>{
+setPage(page+1)
+        }}>Next</button>
       </div>
     </div>
   )
